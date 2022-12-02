2ce49901e9c3b06dfe94034a76ff388d518b27f9
Jui Chi Lee <juichilee@MacBook-Pro.local>
2022-12-03 03:59:40 +0800

    Initialize Spring Boot and Spring Cloud

---
+++ src/main/java/com/dijet/smsUserLoginSystem/SmsUserLoginSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dijet.smsUserLoginSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsUserLoginSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsUserLoginSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dijet/smsUserLoginSystem/SmsUserLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dijet.smsUserLoginSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsUserLoginSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
